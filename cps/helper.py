2a92caaf7ff670967ff95cf0fdbffa1419bed9ea
denn <denn@openweathermap.org>
2018-03-13 19:28:10 +0000

    change file name decode

--- cps/helper.py
+++ cps/helper.py
@@ -48,6 +48,8 @@ try:
 except ImportError:
     use_unidecode = False
 
+use_unidecode = False
+
 # Global variables
 global_task = None
 updater_thread = None
@@ -265,8 +267,10 @@ def get_valid_filename(value, replace_whitespace=True):
     Returns the given string converted to a string that can be used for a clean
     filename. Limits num characters to 128 max.
     """
-    if value[-1:] == u'.':
-        value = value[:-1]+u'_'
+
+    #if value[-1:] == u'.':
+    #    value = value[:-1]+u'_'
+
     value = value.replace("/", "_").replace(":", "_").strip('\0')
     if use_unidecode:
         value = (unidecode.unidecode(value)).strip()
@@ -274,11 +278,11 @@ def get_valid_filename(value, replace_whitespace=True):
         value = value.replace(u'§', u'SS')
         value = value.replace(u'ß', u'ss')
         value = unicodedata.normalize('NFKD', value)
-        re_slugify = re.compile('[\W\s-]', re.UNICODE)
-        if isinstance(value, str):  # Python3 str, Python2 unicode
-            value = re_slugify.sub('', value).strip()
-        else:
-            value = unicode(re_slugify.sub('', value).strip())
+        #re_slugify = re.compile('[\W\s-]', re.UNICODE)
+        #if isinstance(value, str):  # Python3 str, Python2 unicode
+        #    value = re_slugify.sub('', value).strip()
+        #else:
+        #    value = unicode(re_slugify.sub('', value).strip())
     if replace_whitespace:
         #  *+:\"/<>? are replaced by _
         value = re.sub(r'[\*\+:\\\"/<>\?]+', u'_', value, flags=re.U)
@@ -288,6 +292,7 @@ def get_valid_filename(value, replace_whitespace=True):
     if not value:
         raise ValueError("Filename cannot be empty")
 
+    #print(value)
     return value
 
 
